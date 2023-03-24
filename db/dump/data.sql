a1420df9bcea22759ec6efb452a996e2e9ad6d3c
Danil <d.danil.s.96@gmail.com>
2023-03-24 15:23:36 +0500

    add tables dump

---
+++ db/dump/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO clients(login) VALUES
+	('6a0482a2-dce4-491f-a1a8-aeb01a55508a', 'user_one'),
+	('71650b28-e577-45a1-a83a-e97e520be45a', 'user_two'),
+	('be7561a9-aa99-4e12-a78c-995e266ce145', 'user_three');
+	
+INSERT INTO rooms(num) VALUES
+	('97e53db7-f1ac-43dd-a97a-9d7797af2db2', 1),
+	('eeef4bf9-cb9a-48f9-b635-5f9f1d376798', 2),
+	('92377a29-636c-479e-9c02-eb70d56f1756', 3),
+	('f9b179b6-f9c1-4304-aa4a-15d26ad70fde', 4),
+	('1f4ac36c-845e-4440-a219-b9df60ca2cdd', 5);
+	
---
+++ db/dump/tables.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS rooms;
+CREATE TABLE rooms 
+(
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	num integer UNIQUE NOT NULL
+);
+
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients 
+(
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	login varchar(64)
+);
+
+DROP TABLE IF EXISTS reservation;
+CREATE TABLE reservation
+(
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	start_date date NOT NULL,
+	end_date date NOT NULL,
+	vip boolean NOT NULL DEFAULT false,
+	room_id uuid REFERENCES rooms,
+	client_id uuid REFERENCES clients
+);
