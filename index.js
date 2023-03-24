1348c45b3f88106ed7c47dd061f28d0568646c05
Danil <d.danil.s.96@gmail.com>
2023-03-24 16:04:05 +0500

    add find all rooms route

--- index.js
+++ index.js
@@ -6,26 +6,40 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const jsonParser = bodyParser.json();
 
-db.initDb().then(() => {
-    app.get('/rooms', (req, res) => {
-        res.send('All rooms');
-    });
-    
+function asyncCall(callback) {
+    return function (req, res, next) {
+        callback(req, res, next).catch(next)
+    }
+}
+
+function startServer() {
+    app.get('/rooms', asyncCall(async (req, res) => {
+        res.json(await db.findRooms());
+    }));
+
     app.get('/rooms/free/:start/:end', (req, res) => {
         res.send(`Free rooms from ${req.params.start} to ${req.params.end}`);
     });
-    
+
     app.post('/reserve', jsonParser, (req, res) => {
         res.send(`Resrve room ${JSON.stringify(req.body)}`);
     })
-    
+
     app.delete('/reserve', (req, res) => {
         res.send('Remove reservation');
     })
-    
+
     app.listen(PORT, () => {
         console.log(`Example app listening on port ${PORT}`);
     });
-}).catch(() => {
-    console.error('DB migrate failed');
-});
+}
+
+if (process.argv[2] === "--migrate") {
+    db.initDb()
+        .then(startServer)
+        .catch(() => {
+            console.error('DB migrate failed');
+        });
+} else {
+    startServer();
+}
