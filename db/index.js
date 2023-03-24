3f716d1997ecce2b26a38f1ed44d7fd0ec8f965e
Danil <d.danil.s.96@gmail.com>
2023-03-24 15:51:11 +0500

    add db migrate

---
+++ db/index.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const path = require("path");
+const { promises: { readFile } } = require("fs");
+
+const pool = new Pool();
+
+function query(text, params) {
+    return pool.query(text, params);
+}
+
+async function readDump(fileName) {
+    const fileBuffer = await readFile(path.resolve(__dirname, "dump", fileName));
+    return fileBuffer.toString();
+}
+
+async function initDb() {
+    const migrationText = await readDump("tables.sql");
+    const seedText = await readDump("data.sql");
+    await query(migrationText);
+    await query(seedText);
+}
+
+module.exports = { query, initDb };
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const jsonParser = bodyParser.json();
 
-app.get('/rooms', (req, res) => {
-    res.send('All rooms');
-});
-
-app.get('/rooms/free/:start/:end', (req, res) => {
-    res.send(`Free rooms from ${req.params.start} to ${req.params.end}`);
-});
-
-app.post('/reserve', jsonParser, (req, res) => {
-    res.send(`Resrve room ${JSON.stringify(req.body)}`);
-})
-
-app.delete('/reserve', (req, res) => {
-    res.send('Remove reservation');
-})
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`);
+db.initDb().then(() => {
+    app.get('/rooms', (req, res) => {
+        res.send('All rooms');
+    });
+    
+    app.get('/rooms/free/:start/:end', (req, res) => {
+        res.send(`Free rooms from ${req.params.start} to ${req.params.end}`);
+    });
+    
+    app.post('/reserve', jsonParser, (req, res) => {
+        res.send(`Resrve room ${JSON.stringify(req.body)}`);
+    })
+    
+    app.delete('/reserve', (req, res) => {
+        res.send('Remove reservation');
+    })
+    
+    app.listen(PORT, () => {
+        console.log(`Example app listening on port ${PORT}`);
+    });
+}).catch(() => {
+    console.error('DB migrate failed');
 });
