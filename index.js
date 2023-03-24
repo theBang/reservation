cc28eec80fc85eaa4e21f88a2b64b411a8a16539
Danil <d.danil.s.96@gmail.com>
2023-03-24 12:17:33 +0500

    add main endpoints

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/rooms', (req, res) => {
+    res.send('All rooms');
+});
+
+app.get('/rooms/free/:start/:end', (req, res) => {
+    res.send(`Free rooms from ${req.params.start} to ${req.params.end}`);
+});
+
+app.post('/reserve', (req, res) => {
+    res.send('Resrve room');
+})
+
+app.delete('/reserve', (req, res) => {
+    res.send('Remove reservation');
 })
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
-})
+});
