26595f53bd0ec7fe4f46c16221e41d2ac943b445
Danil <d.danil.s.96@gmail.com>
2023-03-27 02:48:13 +0500

    add trg for reserve

--- db/dump/tables.sql
+++ db/dump/tables.sql
@@ -20,6 +20,30 @@ CREATE TABLE reservations
 	start_date date NOT NULL,
 	end_date date NOT NULL,
 	vip boolean NOT NULL DEFAULT false,
-	room_id uuid REFERENCES rooms,
-	client_id uuid REFERENCES clients
+	room_id uuid NOT NULL REFERENCES rooms,
+	client_id uuid NOT NULL REFERENCES clients
 );
+
+DROP FUNCTION IF EXISTS check_room();
+
+CREATE FUNCTION check_room() RETURNS trigger AS $check_room$
+	DECLARE
+		occupiedCount integer;
+	BEGIN
+		SELECT INTO occupiedCount COUNT(room_id) FROM reservations
+			WHERE room_id = NEW.room_id
+				AND (start_date, end_date) OVERLAPS (NEW.start_date, NEW.end_date)
+				AND client_id != NEW.client_id;
+
+		IF occupiedCount > 0 THEN
+            RAISE EXCEPTION 'Room is occupied';
+        END IF;
+		
+		RETURN NEW;
+	END;
+$check_room$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_room 
+	BEFORE INSERT ON reservations
+	FOR EACH ROW 
+	EXECUTE FUNCTION check_room();
