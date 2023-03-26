da147eae6c27306686b71c605036999fa7763bb4
Danil <d.danil.s.96@gmail.com>
2023-03-27 00:16:16 +0500

    add reservation data

--- db/dump/data.sql
+++ db/dump/data.sql
@@ -10,8 +10,7 @@ INSERT INTO rooms(id, num) VALUES
 	('f9b179b6-f9c1-4304-aa4a-15d26ad70fde', 4),
 	('1f4ac36c-845e-4440-a219-b9df60ca2cdd', 5);
 
-INSERT INTO reservations(start_date, end_date, room_id, client_id) 
-	VALUES('9175e01f-2b36-45c1-8b46-ad6cc3e4783f', '2023-04-01', '2023-04-04', '97e53db7-f1ac-43dd-a97a-9d7797af2db2', '6a0482a2-dce4-491f-a1a8-aeb01a55508a');
-INSERT INTO reservations(start_date, end_date, room_id, client_id) 
-	VALUES('9df86f73-f8b5-4c4e-9391-4d85fb2cebe2', '2023-04-05', '2023-04-06', '97e53db7-f1ac-43dd-a97a-9d7797af2db2', '6a0482a2-dce4-491f-a1a8-aeb01a55508a');
-
+INSERT INTO reservations(id, start_date, end_date, room_id, client_id) VALUES
+	('9175e01f-2b36-45c1-8b46-ad6cc3e4783f', '2023-04-01', '2023-04-04', '97e53db7-f1ac-43dd-a97a-9d7797af2db2', '6a0482a2-dce4-491f-a1a8-aeb01a55508a'),
+	('9df86f73-f8b5-4c4e-9391-4d85fb2cebe2', '2023-04-05', '2023-04-06', '97e53db7-f1ac-43dd-a97a-9d7797af2db2', '6a0482a2-dce4-491f-a1a8-aeb01a55508a'),	
+	('054e68c4-c670-43d8-a2bf-df3cfab5f836', '2023-04-01', '2023-04-04', 'eeef4bf9-cb9a-48f9-b635-5f9f1d376798', '71650b28-e577-45a1-a83a-e97e520be45a');
